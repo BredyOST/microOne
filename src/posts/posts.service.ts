5ac9fb51b966e2dab998a0195b4b4169eee92ce6
BredyOST <89292642644m@gmail.com>
2024-02-12 20:13:47 +0300

    доработка функционала

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -449,8 +449,8 @@ export class PostsService {
           //         `https://api.vk.com/method/execute?code=${encodeURIComponent(code)}&access_token=${access}&v=${versionVk}`,
           //     )
         this.httpService.get<any>(`${ip}`, { headers: {
-              'code': `ss`,
-              'version': `ss`,
+              'code': ${encodeURIComponent(code)},
+              'version': versionVk,
               'access': `ss`,
             },
           })
