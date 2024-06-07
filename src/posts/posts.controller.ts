f72e16fa42600e7b0fd2be18914fd3015d74db78
BredyOST <89292642644m@gmail.com>
2024-06-08 00:13:50 +0300

    доработка функционала

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -43,7 +43,7 @@ export class PostsController {
 
     // -----------------------------
     const acceses = [
-      // { token: process.env['ACCESS_TOKEN'] },
+      { token: process.env['ACCESS_TOKEN'] },
       { token: process.env['ACCESS_TOKEN_TWO'] },
     ]
     // стартовый индекс
