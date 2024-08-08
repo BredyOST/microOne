667cdff12680d3ea8c1b002c173e120f89711ac3
BredyOST <89292642644m@gmail.com>
2024-08-08 19:40:36 +0300

    доработка функционала

--- src/fl_ru/fl_ru.controller.ts
+++ src/fl_ru/fl_ru.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { FlRuService } from './fl_ru.service';
 import { Cron } from '@nestjs/schedule';
 import {PostsService} from "../posts/posts.service";
+import process from "process";
 
 @Controller('fl-ru')
 export class FlRuController {
@@ -14,20 +15,23 @@ export class FlRuController {
   @Get('/createPosts')
   async createPosts() {
 
+    const indexWords = process.env['INDEX_WORD'];
     // FLr.ru
     const linksFromFl = [
-      { design: 'https://www.fl.ru/rss/all.xml?category=3', id: 8, index: 'FL' },
-      { design: 'https://freelance.ru/rss/feed/list/s.40', id: 8, index: 'FR' },
+      { design: 'https://www.fl.ru/rss/all.xml?category=3', id: 8, index: 'FL', indexSearch: 1},
+      { design: 'https://freelance.ru/rss/feed/list/s.40', id: 8, index: 'FR', indexSearch: 2 },
     ]
+
+    const linkNext = linksFromFl.filter((item) => +item?.indexSearch == +indexWords)
 
     const categories = await this.flRuService.getCategories();
     const nextCategory = categories?.filter((item) => !item.disabled);
 
-    const endWordsLength = linksFromFl.length
+    const endWordsLength = linkNext?.length
 
     for (let i = 0; i < endWordsLength; i++) {
 
-      const thisItem = linksFromFl[i]
+      const thisItem = linkNext[i]
 
       const category = nextCategory?.find(
           (category) => category?.id == thisItem?.id,
